f820d31fe15326f40a359cce22611968f39f772b
Badal Aryal <badal.aryal@gmail.com>
2023-11-30 11:15:01 +0545

    Showing Question And Answer

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+   
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["The Capital of Nepal is Kathmandu.", "True"],
+        ["The Capital of India is Mumbai.","False"],
+        ["The Capital of China is Bejing.","True"]
+        
+    ]
+    
+    var questionNumber = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateUI()
+        
     }
 
 
+    @IBAction func answerButtonPrssed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle // true, false
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer{
+            print("Right!")
+        }else{
+            print("Wrong!")
+        }
+            
+        if questionNumber < quiz.count - 1{
+            questionNumber += 1
+        }
+        else{
+            questionNumber = 0
+        }
+        updateUI()
+        
+    }
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber][0]
+        
+        
+    }
 }
 
